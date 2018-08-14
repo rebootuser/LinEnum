d481b5a5c2c21cb6e1398c3a24796f3334ac3579
Saeed Hashem <saeedsaeed00093@gmail.com>
2018-08-15 00:43:54 +0800

    Added more POSIX capabilities checks

--- LinEnum.sh
+++ LinEnum.sh
@@ -1058,7 +1058,7 @@ intsuid=`find / -perm -4000 -type f -exec ls -la {} \; 2>/dev/null | grep -w $bi
 		echo -e "\n" 
 	else 
 		:
-	fi
+		fi
   else
 	:
 fi
@@ -1158,6 +1158,67 @@ fileswithcaps=`getcap -r / 2>/dev/null || /sbin/getcap -r / 2>/dev/null`
 	if [ "$fileswithcaps" ]; then
 		echo -e "\e[00;31m[+] Files with POSIX capabilities set:\e[00m\n$fileswithcaps"
 		echo -e "\n"
+	else
+		:
+	fi
+  else
+	  :
+fi
+
+if [ "$thorough" = "1" ]; then
+	if [ "$export" ] && [ "$fileswithcaps" ]; then
+		mkdir $format/files_with_capabilities/ 2>/dev/null
+		for i in $fileswithcaps; do cp $i $format/files_with_capabilities/; done 2>/dev/null
+	else 
+		:
+	fi
+  else
+	  :
+fi
+
+#searches /etc/security/capability.conf for users associated capapilies
+if [ "$thorough" = "1" ]; then
+userswithcaps=`grep -v '^#\|none\|^$' /etc/security/capability.conf 2>/dev/null`
+	if [ "$userswithcaps" ]; then
+		echo -e "\e[00;33m[+] Users with specific POSIX capabilities:\e[00m\n$userswithcaps"
+		echo -e "\n"
+	else
+		:
+	fi
+  else
+	  :
+fi
+
+if [ "$thorough" = "1" ] && [ "$userswithcaps" ] ; then
+#matches the capabilities found associated with users with the current user
+matchedcaps=`echo -e "$userswithcaps" | grep \`whoami\` | awk '{print $1}' 2>/dev/null`
+	if [ "$matchedcaps" ]; then
+		echo -e "\e[00;33m[+] Capabilities associated with the current user:\e[00m\n$matchedcaps"
+		echo -e "\n"
+		#matches the files with capapbilities with capabilities associated with the current user
+		matchedfiles=`echo -e "$matchedcaps" | while read -r cap ; do echo -e "$fileswithcaps" | grep "$cap" ; done 2>/dev/null`
+		if [ "$matchedfiles" ]; then
+			echo -e "\e[00;33m[+] Files with the same capabilities associated with the current user (You may want to try abusing those capabilties):\e[00m\n$matchedfiles"
+			echo -e "\n"
+			#lists the permissions of the files having the same capabilies associated with the current user
+			matchedfilesperms=`echo -e "$matchedfiles" | awk '{print $1}' | while read -r f; do ls -la $f ;done 2>/dev/null`
+			echo -e "\e[00;33m[+] Permissions of files with the same capabilities associated with the current user:\e[00m\n$matchedfilesperms"
+			echo -e "\n"
+			if [ "$matchedfilesperms" ]; then
+				#checks if any of the files with same capabilities associated with the current user is writable
+				writablematchedfiles=`echo -e "$matchedfiles" | awk '{print $1}' | while read -r f; do find $f -writable -exec ls -la {} + ;done 2>/dev/null`
+				if [ "$writablematchedfiles" ]; then
+					echo -e "\e[00;33m[+] User/Group writable files with the same capabilities associated with the current user:\e[00m\n$writablematchedfiles"
+					echo -e "\n"
+				else
+					:
+				fi
+			else
+				:
+			fi
+		else
+			:
+		fi
 	else
 		:
 	fi
