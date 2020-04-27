2a370a831d853695e3c2c3693463df21d8ee05c8
jsav0 <jas.savage@gmail.com>
2020-04-27 04:36:46 -0400

    fixed missing || or bracket

--- LinEnum.sh
+++ LinEnum.sh
@@ -469,7 +469,7 @@ fi
   # include inactive timers in thorough mode
   systemdtimers="$(systemctl list-timers --all 2>/dev/null)"
   info=""
-else
+} || {
   systemdtimers="$(systemctl list-timers 2>/dev/null |head -n -1 2>/dev/null)"
   # replace the info in the output with a hint towards thorough mode
   info="\e[2mEnable thorough tests to see inactive timers\e[00m"
